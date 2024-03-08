72494e36bc7cbe213dd1528fd36cfc36c576eb1c
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-08 11:29:01 +0100

    refactor: moved Todo declaration to src/declarations.ts

--- src/todosContext/Context.ts
+++ src/todosContext/Context.ts
@@ -1,10 +1,5 @@
 import { createContext } from "react";
 
-interface Todo {
-  title: string;
-  completed: boolean;
-}
-
 const todoContext = createContext<Todo | null>(null);
 
 export default todoContext;

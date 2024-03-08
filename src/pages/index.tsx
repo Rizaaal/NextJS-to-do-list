aa6c8227f4557818b010e84882969616da6364c8
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-08 13:47:33 +0100

    refactor: removed unused variables and imports

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,4 @@
 import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useContext } from "react";
@@ -9,7 +8,7 @@ import Todo from "@/components/Todo";
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
-  const { todos, setTodos } = useContext(todoContext);
+  const { todos } = useContext(todoContext);
 
   return (
     <>

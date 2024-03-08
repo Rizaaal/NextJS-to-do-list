11919675f7541b4e83d282b6be507c41a001046c
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-08 13:04:41 +0100

    feat: added deleteTodo() function

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -2,31 +2,45 @@ import { Dispatch, ReactElement, SetStateAction, createContext, useEffect, useSt
 
 export const todoContext = createContext<ContextProps>({
   todos: [],
-  setTodos: () => {}
+  setTodos: () => {},
+  deleteTodo: () => {}
 });
 
 export interface ContextProps {
   todos: Todo[] | null
   setTodos: Dispatch<SetStateAction<Todo[] | null>>
+  deleteTodo: (id: number) => void
 }
 
 
 
-export default function TodoContext({ children }: {children: ReactElement}){
+export default function TodoContext({ children }: { children: ReactElement }) {
   const [todos, setTodos] = useState<Todo[] | null>(null)
 
-  function getTodos(){
+  function getTodos() {
     fetch('https://jsonplaceholder.typicode.com/posts')
-    .then(response => response.json())
-    .then(todos => setTodos(todos))
+      .then(response => response.json())
+      .then(todos => setTodos(todos))
   }
-  
+
+  function deleteTodo(id: number) {
+    console.log('deleteTodo');
+    
+    const prevTodos: Todo[] | null = todos;
+    if (prevTodos !== null) {
+      const newTodos = prevTodos.filter((todo: Todo) => todo.id !== id);
+      setTodos(newTodos);
+    } else {
+      console.log('error: todos not found');
+    }
+  };
+
   useEffect(() => {
     getTodos()
   }, [])
 
   return (
-    <todoContext.Provider value={{ todos, setTodos }}>
+    <todoContext.Provider value={{ todos, setTodos, deleteTodo }}>
       {children}
     </todoContext.Provider>
   );

41189a48b148c50d4f8f1d927907b25023e1542d
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-08 13:46:31 +0100

    feat: implemented check completed on Todo component

--- src/components/Todo.tsx
+++ src/components/Todo.tsx
@@ -2,18 +2,19 @@ import { todoContext } from "@/ContextProvider";
 import { useContext } from "react";
 
 export default function Todo({title, body, id}: {title: string, body: string, id: number}){
-  const { deleteTodo } = useContext(todoContext);
+  const { deleteTodo, completed, setCompleteValue } = useContext(todoContext);
 
   const div = {
     border: 'solid 1px white',
     padding: '1em'
-  }
+  }  
 
   return (
     <div style={div}>
       <h1>{title}</h1>
       <p>{body}</p>
-      <p>completed: <input type="checkbox" /></p>
+      <p>completed: <input type="checkbox" checked={completed[id] ? true : false} onChange={() => setCompleteValue(id)}/>
+      </p>
       <button onClick={() => deleteTodo(id)}>Delete todo</button>
     </div>
   );

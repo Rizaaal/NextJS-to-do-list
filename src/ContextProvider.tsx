0e8f54367618e2f31132128a7af89eab696f0c00
Gabriele Di Grazia <gabrii539@gmail.com>
2024-03-08 12:07:14 +0100

    feat: imported todos with placeholder

--- src/ContextProvider.tsx
+++ src/ContextProvider.tsx
@@ -1,3 +1,4 @@
+import { Dispatch, ReactElement, SetStateAction, createContext, useEffect, useState } from "react";
 
 export const todoContext = createContext<ContextProps | null>(null);
 
@@ -9,3 +10,21 @@ export interface ContextProps {
 
 
 export default function TodoContext({ children }: {children: ReactElement}){
+  const [todos, setTodos] = useState<Todo[] | null>(null)
+
+  function getTodos(){
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(todos => setTodos(todos))
+  }
+  
+  useEffect(() => {
+    getTodos()
+  }, [])
+
+  return (
+    <todoContext.Provider value={{ todos, setTodos }}>
+      {children}
+    </todoContext.Provider>
+  );
+};
